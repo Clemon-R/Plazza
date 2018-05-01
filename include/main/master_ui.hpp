b32a1a6e261192e44e416fab5378c3bb937b4581
Antoine Hartwig <antoine.hartwig@epitech.eu>
2018-05-01 19:18:27 +0200

    Adding start menu

--- include/main/master_ui.hpp
+++ include/main/master_ui.hpp
@@ -16,8 +16,15 @@ class master_ui : public master
 public:
 	master_ui(const int);
 	void	run_interface() override final;
+	void	run_menu();
 	bool	init_graphic();
+	void	init_choices();
+	void	draw_choice(bool);
 private:
 	std::unique_ptr<sfml>	_graphic;
+	std::string	_file;
+	sf::RenderWindow	_window;
+	sf::Font	_font;
+	sf::Text	choice[2];
 };
 #endif /* !MASTER_UI_HPP_ */
--- src/main/master_ui.cpp
+++ src/main/master_ui.cpp
@@ -10,7 +10,6 @@
 
 master_ui::master_ui(const int max) : master(max)
 {
-
 }
 
 bool	master_ui::init_graphic()
@@ -22,7 +21,74 @@ bool	master_ui::init_graphic()
 	return (true);
 }
 
+void	master_ui::draw_choice(bool whichOne)
+{
+	if (whichOne){
+		choice[0].setStyle(sf::Text::Underlined);
+		choice[1].setStyle(sf::Text::Regular);
+	}
+	else{
+		choice[0].setStyle(sf::Text::Regular);
+		choice[1].setStyle(sf::Text::Underlined);
+	}
+	_window.draw(choice[0]);
+	_window.draw(choice[1]);
+}
+
+void	master_ui::init_choices()
+{
+	sf::FloatRect	rect;
+
+	if (!_font.loadFromFile("fonts/font1.ttf"))
+		exit(84);
+	choice[0].setFont(_font);
+	choice[0].setString("Select file to scrap");
+	choice[0].setCharacterSize(20);
+	rect = choice[0].getLocalBounds();
+	choice[0].setOrigin(rect.left + rect.width / 2.0f,
+		rect.top + rect.height / 2.0f);
+	choice[0].setPosition(sf::Vector2f(_window.getSize().x / 2.0f,
+		_window.getSize().y / 2.0f - 40));
+	choice[1].setFont(_font);
+	choice[1].setString("Enter commands manually");
+	choice[1].setCharacterSize(20);
+	rect = choice[1].getLocalBounds();
+	choice[1].setOrigin(rect.left + rect.width / 2.0f,
+		rect.top + rect.height / 2.0f);
+	choice[1].setPosition(sf::Vector2f(_window.getSize().x / 2.0f,
+		_window.getSize().y / 2.0f));
+}
+
+void	master_ui::run_menu()
+{
+	sf::Event event;
+	sf::Vector2i	pos;
+	bool	whichOne = true;
+
+	pos.x = sf::VideoMode::getDesktopMode().width / 2 - 540;
+	pos.y = sf::VideoMode::getDesktopMode().height / 2 - 420;
+	_window.create(sf::VideoMode(1080, 840), "The Plazza");
+	_window.setPosition(pos);
+	init_choices();
+	while (_window.isOpen()){
+		while (_window.pollEvent(event)){
+			if (event.type == sf::Event::Closed)
+				_window.close();
+			if (event.key.code == sf::Keyboard::Escape)
+				_window.close();
+			if (event.key.code == sf::Keyboard::Down)
+				whichOne = false;
+			if (event.key.code == sf::Keyboard::Up)
+				whichOne = true;
+		}
+		_window.clear(sf::Color::Black);
+		draw_choice(whichOne);
+		_window.display();
+	}
+	_run = false;
+}
+
 void	master_ui::run_interface()
 {
-	
+	run_menu();
 }
