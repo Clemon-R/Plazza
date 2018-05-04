587c998bcb2d742dc7968ddb6665b8a6b3e0697f
Antoine Hartwig <antoine.hartwig@epitech.eu>
2018-05-04 18:44:20 +0200

    Need to pimp interface, but especially need to link the interface with the core

--- src/main/process/master/master_ui.cpp
+++ src/main/process/master/master_ui.cpp
@@ -9,6 +9,31 @@
 #include "main/process/master/master_ui.hpp"
 #include <dirent.h>
 
+/* Utility functions */
+
+void	master_ui::my_set_position(sf::Text *text, int gap)
+{
+	sf::FloatRect	rect;
+
+	rect = text->getLocalBounds();
+	text->setOrigin(rect.left + rect.width / 2.0f,
+		rect.top + rect.height / 2.0f);
+	text->setPosition(sf::Vector2f(_window.getSize().x / 2.0f,
+		_window.getSize().y / 2.0f + gap));
+}
+
+std::string	string_converter(char *old_string)
+{
+	std::string	new_string(old_string);
+
+	return (new_string);
+}
+
+static bool	compare_commands_ui(const command &first, const command &second)
+{
+	return (true);
+}
+
 /* Constructor */
 
 master_ui::master_ui(const int max) : master(max)
@@ -36,11 +61,15 @@ void	master_ui::init_first_page()
 	first_page_choices[0].setFont(_font);
 	first_page_choices[0].setString("Select file to scrap");
 	first_page_choices[0].setCharacterSize(20);
-	my_set_position(&first_page_choices[0], -20);
+	my_set_position(&first_page_choices[0], -40);
 	first_page_choices[1].setFont(_font);
-	first_page_choices[1].setString("Quit");
+	first_page_choices[1].setString("Enter commands manuallly");
 	first_page_choices[1].setCharacterSize(20);
-	my_set_position(&first_page_choices[1], 20);
+	my_set_position(&first_page_choices[1], 0);
+	first_page_choices[2].setFont(_font);
+	first_page_choices[2].setString("Quit");
+	first_page_choices[2].setCharacterSize(20);
+	my_set_position(&first_page_choices[2], 40);
 }
 
 void	master_ui::init_second_page()
@@ -59,25 +88,6 @@ void	master_ui::init_second_page()
 	closedir(dir);
 }
 
-/* Utility functions */
-
-void	master_ui::my_set_position(sf::Text *text, int gap)
-{
-	sf::FloatRect	rect;
-
-	rect = text->getLocalBounds();
-	text->setOrigin(rect.left + rect.width / 2.0f,
-		rect.top + rect.height / 2.0f);
-	text->setPosition(sf::Vector2f(_window.getSize().x / 2.0f,
-		_window.getSize().y / 2.0f + gap));
-}
-
-std::string	master_ui::string_converter(char *old_string)
-{
-	std::string	new_string(old_string);
-
-	return (new_string);
-}
 
 /* First page */
 
@@ -86,12 +96,19 @@ void	master_ui::draw_first_page(int whichOne)
 	if (whichOne == 0){
 		first_page_choices[0].setStyle(sf::Text::Underlined);
 		first_page_choices[1].setStyle(sf::Text::Regular);
+		first_page_choices[2].setStyle(sf::Text::Regular);
 	}else if (whichOne == 1){
 		first_page_choices[0].setStyle(sf::Text::Regular);
 		first_page_choices[1].setStyle(sf::Text::Underlined);
+		first_page_choices[2].setStyle(sf::Text::Regular);
+	}else if (whichOne == 2){
+		first_page_choices[0].setStyle(sf::Text::Regular);
+		first_page_choices[1].setStyle(sf::Text::Regular);
+		first_page_choices[2].setStyle(sf::Text::Underlined);
 	}
 	_window.draw(first_page_choices[0]);
 	_window.draw(first_page_choices[1]);
+	_window.draw(first_page_choices[2]);
 }
 
 void	master_ui::first_page()
@@ -104,12 +121,14 @@ void	master_ui::first_page()
 			sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 			_window.close();
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-			whichOne += (whichOne > 0) ? -1 : 1;
+			whichOne += (whichOne > 1) ? -2 : 1;
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-			whichOne -= (whichOne < 1) ? -1 : 1;
+			whichOne -= (whichOne < 1) ? -2 : 1;
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)){
 			if (whichOne == 0)
 				_actual_page = 1;
+			else if (whichOne == 1)
+				_actual_page = 2;
 			else
 				_window.close();
 		}
@@ -119,27 +138,82 @@ void	master_ui::first_page()
 
 /* Second page */
 
-void	master_ui::draw_second_page()
+void	master_ui::draw_second_page(int whichOne)
 {
-	sf::Text	test;
+	sf::Text	text;
+	int	pos = -20 * _file_list.size();
+	int	actual_printed = 0;
 
-	test.setFont(_font);
-	test.setString("This is the second page");
-	test.setCharacterSize(20);
-	my_set_position(&test, 0);
-	_window.draw(test);
+	for(auto inside : _file_list){
+		text.setFont(_font);
+		text.setString(inside);
+		text.setCharacterSize(20);
+		if (actual_printed == whichOne)
+			text.setStyle(sf::Text::Underlined);
+		else
+			text.setStyle(sf::Text::Regular);
+		my_set_position(&text, pos);
+		_window.draw(text);
+		pos += 40;
+		actual_printed += 1;
+	}
 }
 
 void	master_ui::second_page()
 {
 	sf::Event	event;
+	int	nb_element = _file_list.size();
+	static int	whichOne = 0;
 
 	while (_window.pollEvent(event)){
 		if (event.type == sf::Event::Closed ||
 			sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-			_window.close();
+			_actual_page = 0;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			whichOne += (whichOne > nb_element - 2) ? -nb_element + 1 : 1;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			whichOne -= (whichOne < 1) ? -nb_element + 1: 1;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+			_commands.merge(commandParser::parse_line(
+				_file_list.at(whichOne)), compare_commands_ui);
 	}
-	draw_second_page();
+	draw_second_page(whichOne);
+}
+
+/* Third page */
+
+void	master_ui::draw_third_page()
+{
+
+}
+
+void	master_ui::third_page()
+{
+	std::size_t	size = 0;
+	sf::Event	event;
+	static sf::String	entry;
+	static sf::Text	to_display;
+
+	while (_window.pollEvent(event)){
+		if (event.type == sf::Event::Closed ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			_actual_page = 0;
+		if (event.type == sf::Event::TextEntered){
+			if (event.text.unicode > 31 && event.text.unicode < 128){
+				entry += event.text.unicode;
+				std::cout << entry.toAnsiString() << std::endl;
+			}
+			else if (event.text.unicode == 13){
+				std::cout << entry.toAnsiString() << std::endl;
+				to_display.setFont(_font);
+				to_display.setCharacterSize(20);
+				to_display.setString(entry);
+				my_set_position(&to_display, 0);
+				entry.clear();
+			}
+		}
+	}
+	_window.draw(to_display);
 }
 
 /* Menu */
@@ -150,9 +224,11 @@ void	master_ui::run_menu()
 		first_page();
 	else if (_actual_page == 1)
 		second_page();
+	else if (_actual_page == 2)
+		third_page();
 }
 
-void	master_ui::run_interface()
+void	master_ui::run_interface(std::mutex &lock)
 {
 	sf::Vector2i	pos;
 
